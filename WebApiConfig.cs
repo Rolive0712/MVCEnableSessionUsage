41165f19be242132636961cffab8814f20b25094
Rolive0712 <roshitrajan@rediffmail.com>
2014-09-14 16:50:58 -0400

    Create WebApiConfig.cs

---
+++ WebApiConfig.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using CAMPDashboard.Code.Helpers;
+using System.Web.Routing;
+
+namespace <ns>
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            //config.Routes.MapHttpRoute(
+            //       name: "ActionApi",
+            //       routeTemplate: "api/{controller}/{action}/{id}",
+            //       defaults: new { id = RouteParameter.Optional }//,
+            //    //constraints: new { id = @"^[0-9]+$" }
+            //   );
+
+            RouteTable.Routes.MapHttpRoute(
+                name: "ActionApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            ).RouteHandler = new SessionRouteHandler();
+
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
